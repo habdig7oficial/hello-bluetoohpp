10e393abd590d11dd04329dc7f2d57bdfb5aa897
Mateus Vieira <habdig7@gmail.com>
2024-09-25 17:28:22 -0300

    lastest

--- lcd.ino
+++ lcd.ino
@@ -15,23 +15,46 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <RTClib.h>
 
-#define ardRX  1
-#define ardTX  2
-#define ardSNS 3
+#define MPU_I2CADDR_AD0_HIGH 0x69
 
-#define CR     0x0d        // Cariage Return
-#define LF     0x0a        // Line Feed
+#define ardRX   1
+#define ardTX   2
+#define ardSNS  3
+
+#define led_1   22
+#define led_2   23
+#define led_3   24
+#define led_4   25
+
+#define CR      0x0d        // Cariage Return
+#define LF      0x0a        // Line Feed
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // pinos do LCD - RS E D4 D5 D6 D7
 
 Adafruit_MPU6050 mpu;
 
-int i = 0;
+RTC_DS1307 rtc;
+
+char hora[6]    = { 53, 52, 51, 50, 49, 48 };
+char minuto[6]  = { 47, 46, 45, 44, 43, 42 };
+char segundo[6] = { 41, 40, 39, 38, 37, 36 };
+
+int h = 0;
+int m = 0;
+int s = 0;
+int ms = 0;
+
 int pos = 32;
 int direction = ardRX;
 char str[49] = "                                                     ";
 char c;
+
+int l1 = LOW;
+int l2 = LOW;
+int l3 = LOW;
+int l4 = LOW;
 
 char buf[2][40] = { "# MackTempo #\xd\xaSeja bem vindo!!\xd\xa",
                     "Quem é você?\xd\xa"
@@ -41,6 +64,7 @@ int linha = 0;
 
 char alerta[8] = "Caiu!\xd\xa";
 int  l = 0;
+int caiu = 0;
 
 sensors_event_t a, g, temp;
 sensors_event_t a_old, g_old, temp_old;
@@ -48,10 +72,26 @@ sensors_event_t a_old, g_old, temp_old;
 
 void setup()
 {
+  int i;
+
   lcd.begin(16, 2); // inicializa LCD 16x2
   lcd.clear(); // limpa tela do LCD
 
-  Serial.begin(9600); // Inicia o monitor serial via USB
+  pinMode (led_1, OUTPUT);
+  pinMode (led_2, OUTPUT);
+  pinMode (led_3, OUTPUT);
+  pinMode (led_4, OUTPUT);
+
+  for (i=0; i < 6; i++)
+  {
+    if (i != 5)
+      pinMode (hora[i], OUTPUT);
+    pinMode (minuto[i], OUTPUT);
+    pinMode (segundo[i], OUTPUT);
+  }
+
+  //Serial.begin(115200); // Inicia o monitor serial via USB
+  Serial.begin(115200);
   while (!Serial)
     delay (10);
 
@@ -59,16 +99,28 @@ void setup()
   while (!Serial1)
     delay (10);
 
+  if (!rtc.begin())
+  {
+    Serial.println ("RTC nao encontrado");
+    while (1);
+  }
+
+  if (!rtc.isrunning())
+  {
+    Serial.println ("ajuste do RTC pelo PC");
+    rtc.adjust (DateTime(F(__DATE__), F(__TIME__)));
+  }
+
   // Tentando inicializar
   Serial.println("MPU6050 teste!");
-  if (!mpu.begin()) 
+  if (!mpu.begin(MPU_I2CADDR_AD0_HIGH)) 
   { 
     // Se caso o sensor não iniciar
     Serial.println ("A inicialização sensor MPU-6050 falhou");
     while (1) {} // Se a inicialização falhar, o programa irá travar aqui
   }
   Serial.println("MPU6050 encontrado!");
-
+  
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G); // Definindo o range de ação do acelerometro.
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);      // Definindo o range de ação do giroscopio.
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);   // Configurando a largura da banda do filtro
@@ -79,42 +131,153 @@ void setup()
   delay(100);
 }
 
+
+
+void acende_led (char *vet, int mask, int valor)
+{
+  int i;
+
+  for (i=0; mask; i++)
+  {
+    if (valor & mask)
+      digitalWrite (vet[i], HIGH);
+    else
+      digitalWrite (vet[i], LOW);  
+    mask >>= 1;
+  }
+}
+
+
+
 void loop()
 {
+  int i;
+
+  DateTime now = rtc.now();
+
+  acende_led (hora,    0x10, now.hour());
+  acende_led (minuto,  0x20, now.minute());
+  acende_led (segundo, 0x20, now.second());
+
   /*
-  if (direction == ardRX)
-    if (Serial1.available())
-    { 
-      // Lendo os caracteres enviados
-      c = Serial1.read ();
-      //Serial.print (c);
+  acende_led (hora,    0x10, h);
+  acende_led (minuto,  0x20, m);
+  acende_led (segundo, 0x20, s);
 
-      if (c != CR && c != LF)
+  ms++;
+  if (ms == 5 )
+  {
+    ms = 0;
+    s++;
+    if (s == 60)
+    {
+      s = 0;
+      m++;
+      if (m == 60)
       {
-         str[pos++] = c;
-        if (pos >= 48)
-          pos = 32;
+        m = 0;
+        h++;
+        if (h == 24)
+          h = 0;
       }
+    }
+  }
+  */
+  delay (10);
 
-      if (c == LF)
+
+  l1 = (l1 == HIGH)? LOW : HIGH;
+ // digitalWrite (led_1, l1);
+
+  mpu.getEvent(&a, &g, &temp);
+
+  //if (abs(a.acceleration.x - a_old.acceleration.x) > 0.2 || abs(a.acceleration.y-a_old.acceleration.y) > 0.2 || abs(a.acceleration.z-a_old.acceleration.z) > 0.2)
+  if (abs(g.gyro.z-g_old.gyro.z) > 0.5)
+  {
+    Serial1.println(++caiu);
+
+    // Imprimindo no monitor Serial os valores capturados
+    Serial.print("Aceleracao em X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotacao em X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+      
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" oC");
+    Serial.println("");
+
+    Serial.print ("Caiu ");
+    Serial.println(caiu);
+
+    //direction = ardSNS;
+  }
+
+  a_old = a;
+  g_old = g;
+  temp_old = temp;
+
+
+
+  //digitalWrite (led_2, l1);
+
+  //digitalWrite (led_2, l2);
+  //l2 = (l2 == HIGH)? LOW : HIGH;
+
+  if (Serial1.available())
+  { 
+    // Lendo os caracteres enviados
+    c = Serial1.read ();
+    //Serial.print (c);
+
+    if (c != CR && c != LF)
+    {
+      str[pos++] = c;
+      if (pos >= 48)
       {
-        while (pos < 48)
-          str[pos++] = 32;
-
         for (i=0; i<32; i++)
           str[i] = str[i + 16];
-
         pos = 32;
-        direction = ardTX;
       }
-
-      lcd.setCursor(0, 0); // selecionando coluna 0 e linha 0
-      lcd.print(str);
-      lcd.setCursor(0, 1);
-      lcd.print(str+16);'
     }
-*/
-  if (direction == ardTX)
+
+    if (c == LF && pos != 32)
+    {
+      while (pos < 48)
+        str[pos++] = 32;
+
+      for (i=0; i<32; i++)
+        str[i] = str[i + 16];
+
+      pos = 32;
+      direction = ardTX;
+    }
+
+    lcd.setCursor(0, 0); // selecionando coluna 0 e linha 0
+    lcd.print(str);
+    lcd.setCursor(0, 1);
+    lcd.print(str+16);
+  }
+
+
+
+ // digitalWrite (led_3, l1);
+
+  //digitalWrite (led_3, l3);
+  //l3 = (l3 == HIGH)? LOW : HIGH;
+
+  while (direction == ardTX)
     if (Serial1.availableForWrite())
     {
       Serial1.write(buf[linha][count++]);
@@ -128,59 +291,28 @@ void loop()
       }
     }
 
-  if (direction == ardSNS)
-  {
+
+
+  //digitalWrite (led_4, l1);
+
+  /*
+  //digitalWrite (led_4, l4);
+  //l4 = (l4 == HIGH)? LOW : HIGH;
+
+  while (direction == ardSNS)
     if (Serial1.availableForWrite())
+    {
       Serial1.write(alerta[l++]);
-    if (l >= 7)
-    {
-      l = 0;
-      direction = ardRX;
-      count = 0;
-      linha = 0;
-      pos = 32;
-      for (i = 0; i < sizeof(str) - 1; i++)
-        str[i] = ' ';
+      if (l >= 7)
+      {
+        l = 0;
+        direction = ardRX;
+        count = 0;
+        linha = 0;
+        pos = 32;
+        for (i = 0; i < sizeof(str) - 1; i++)
+          str[i] = ' ';
+      }
     }
-  }
-  else
-  {
-    mpu.getEvent(&a, &g, &temp);
-
-    //if (g.gyro.x!=g_old.gyro.x || g.gyro.y!=g_old.gyro.y || g.gyro.z!=g_old.gyro.z)
-    //if (abs(a.acceleration.x - a_old.acceleration.x) > 0.1 || abs(a.acceleration.y-a_old.acceleration.y) > 0.1 || abs(a.acceleration.z-a_old.acceleration.z) > 0.1)
-    if (abs(g.gyro.z-g_old.gyro.z) > 0.5)
-    {
-      /*
-      // Imprimindo no monitor Serial os valores capturados
-      Serial.print("Aceleracao em X: ");
-      Serial.print(a.acceleration.x);
-      Serial.print(", Y: ");
-      Serial.print(a.acceleration.y);
-      Serial.print(", Z: ");
-      Serial.print(a.acceleration.z);
-      Serial.println(" m/s^2");
-
-      Serial.print("Rotacao em X: ");
-      Serial.print(g.gyro.x);
-      Serial.print(", Y: ");
-      Serial.print(g.gyro.y);
-      Serial.print(", Z: ");
-      Serial.print(g.gyro.z);
-      Serial.println(" rad/s");
-
-      Serial.print("Temperature: ");
-      Serial.print(temp.temperature);
-      Serial.println(" oC");
-      Serial.println("");
-      */
-
-      //Serial.println ("Caiu!");
-      direction = ardSNS;
-    }
-
-    a_old = a;
-    g_old = g;
-    temp_old = temp; 
-  }
+  */
 }
